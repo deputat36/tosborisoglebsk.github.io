2f841d52bffef15190448d69d78a5a13f57df30a
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2025-12-20 10:34:12 +0300

    Create tos.geo.js

---
+++ data/tos.geo.js
@@ -0,0 +1,7 @@
+window.DATA = window.DATA || {};
+// Добавьте сюда GeoJSON FeatureCollection. Пример feature.properties:
+// { name: "Уютный", place: "г. Борисоглебск", id: "uyutnyy" }
+window.DATA.tosGeo = {
+  "type": "FeatureCollection",
+  "features": []
+};
