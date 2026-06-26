ff1850849daf901aaec13a779fb09964b328fd66
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-26 21:36:53 +0300

    Include projects integrity in project audit

--- scripts/audit_project_mode.js
+++ scripts/audit_project_mode.js
@@ -8,6 +8,7 @@ const checks = [
   ['Path checks', 'scripts/test_path_checks.js'],
   ['Status sets', 'scripts/test_status_sets.js'],
   ['TOS integrity', 'scripts/audit_tos_integrity.js'],
+  ['Projects integrity', 'scripts/audit_projects_integrity.js'],
   ['Autonomous improvement plan', 'scripts/audit_autonomous_improvement_plan.js'],
   ['User decision queue', 'scripts/audit_user_decision_queue.js'],
   ['Manual tasks', 'scripts/audit_github_manual_tasks.js'],
