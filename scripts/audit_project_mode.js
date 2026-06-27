e43347c5fcb727f24eff2e7d748a946fb1e5ecc4
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-27 21:53:30 +0300

    Include partners page content in project audit

--- scripts/audit_project_mode.js
+++ scripts/audit_project_mode.js
@@ -18,6 +18,7 @@ const checks = [
   ['Legal page content', 'scripts/audit_legal_page_content.js'],
   ['Contacts page content', 'scripts/audit_contacts_page_content.js'],
   ['Residents page content', 'scripts/audit_residents_page_content.js'],
+  ['Partners page content', 'scripts/audit_partners_page_content.js'],
   ['Projects 2026 workflow', 'scripts/audit_projects_2026_workflow.js'],
   ['Needs integrity', 'scripts/audit_needs_integrity.js'],
   ['Needs pages content', 'scripts/audit_needs_pages_content.js'],
