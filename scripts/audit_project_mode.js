456c55bf6eb5418aece485e0021fbf92225696a4
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-28 12:11:03 +0300

    Include chairperson first 30 days in project audit

--- scripts/audit_project_mode.js
+++ scripts/audit_project_mode.js
@@ -19,6 +19,7 @@ const checks = [
   ['Legal page content', 'scripts/audit_legal_page_content.js'],
   ['Contacts page content', 'scripts/audit_contacts_page_content.js'],
   ['Chairperson page content', 'scripts/audit_chairperson_page_content.js'],
+  ['Chairperson first 30 days content', 'scripts/audit_chairperson_first_30_days_content.js'],
   ['Chairperson action routes content', 'scripts/audit_chairperson_action_routes_content.js'],
   ['Residents page content', 'scripts/audit_residents_page_content.js'],
   ['Residents action routes content', 'scripts/audit_residents_action_routes_content.js'],
