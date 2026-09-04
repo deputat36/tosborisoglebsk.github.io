e17273c3789e40ee328b27f689b597b9684987bb
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-04 15:47:41 +0300

    Закрепить origins Ипаса и Северного-41

--- scripts/lib/content_origin.js
+++ scripts/lib/content_origin.js
@@ -21,7 +21,9 @@ const VERIFIED_NEWS_IDS = new Set([
   'pervomayskiy-stage-competition-2021',
   'khoperskiy-bereg-sports-ground-competition-2021',
   'znamenie-created-2024',
-  'uyutny-cultural-festival-2025'
+  'uyutny-cultural-festival-2025',
+  'severnyy-41-territory-competition-2021',
+  'ipas-playground-application-2023'
 ]);
 
 const ORIGIN_LABELS = {
