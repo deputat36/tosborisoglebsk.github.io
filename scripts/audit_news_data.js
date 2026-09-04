af8261cb336b0df81961f3802f9a830182cb7f3f
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-04 15:40:39 +0300

    Защитить обязательные source-backed новости wave 12

--- scripts/audit_news_data.js
+++ scripts/audit_news_data.js
@@ -22,7 +22,13 @@ const requiredNewsIds = [
   'severnyy-39-playground-2021',
   'ulyanovka-sports-projects-2020',
   'ivanovka-lighting-competition-2021',
-  'podstepki-cemetery-fence-competition-2021'
+  'podstepki-cemetery-fence-competition-2021',
+  'chigorak-pedestrian-bridge-2019',
+  'port-artur-cemetery-competition-2021',
+  'pervomayskiy-stage-competition-2021',
+  'khoperskiy-bereg-sports-ground-competition-2021',
+  'znamenie-created-2024',
+  'uyutny-cultural-festival-2025'
 ];
 
 function isObject(value) {
