0fb29fe62e0a0b729e72bb7bf99486110a44e045
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-04 15:56:01 +0300

    Синхронизировать required news с текущим каталогом

--- scripts/audit_news_data.js
+++ scripts/audit_news_data.js
@@ -23,13 +23,10 @@ const requiredNewsIds = [
   'ulyanovka-sports-projects-2020',
   'ivanovka-lighting-competition-2021',
   'podstepki-cemetery-fence-competition-2021',
-  'chigorak-pedestrian-bridge-2019',
   'port-artur-cemetery-competition-2021',
   'pervomayskiy-stage-competition-2021',
-  'khoperskiy-bereg-sports-ground-competition-2021',
   'znamenie-created-2024',
   'uyutny-cultural-festival-2025',
-  'severnyy-41-territory-competition-2021',
   'ipas-playground-application-2023'
 ];
 
