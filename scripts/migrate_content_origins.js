2511500bb2de6550610165651d4377897f505e3e
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-10 22:19:04 +0300

    Run verified Chkalovets project generator during content migration

--- scripts/migrate_content_origins.js
+++ scripts/migrate_content_origins.js
@@ -14,6 +14,7 @@ require('./generate_verified_done_wave3');
 require('./generate_chairperson_articles_wave1');
 require('./generate_verified_mirolyubie_memory_project');
 require('./generate_verified_kalinka_wells_project');
+require('./generate_verified_chkalovec_projects_wave1');
 
 const ROOT = process.cwd();
 const COLLECTIONS = ['news', 'projects', 'needs', 'done', 'articles'];
