cae03efc0d08a67fe1cc268b78abbe9fc964e3a1
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-10 21:41:17 +0300

    Include chairperson articles in content migration

--- scripts/migrate_content_origins.js
+++ scripts/migrate_content_origins.js
@@ -11,6 +11,7 @@ require('./generate_verified_projects_wave4');
 require('./generate_verified_done_wave1');
 require('./generate_verified_done_wave2');
 require('./generate_verified_done_wave3');
+require('./generate_chairperson_articles_wave1');
 
 const ROOT = process.cwd();
 const COLLECTIONS = ['news', 'projects', 'needs', 'done', 'articles'];
