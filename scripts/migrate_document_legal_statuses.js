762e0f29920c75d95993350878c24ea260077ee4
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-09-10 21:36:08 +0300

    Include chairperson routine documents in migration

--- scripts/migrate_document_legal_statuses.js
+++ scripts/migrate_document_legal_statuses.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+require('./generate_chairperson_routine_documents');
 
 const documentsPath = path.join(process.cwd(), 'data', 'documents.json');
 
